ad48d2eacb61f0c4dc2b8394266ea00526e7e54a
Danny <danny@theforbiddenarts.com>
2017-07-08 02:43:12 -0400

    Added Status bar and more commenting. Removed file path lookup call(Needs to be a function that returns path)

--- myFirstCustomGUI.py
+++ myFirstCustomGUI.py
@@ -4,20 +4,36 @@ from tkinter import filedialog
 
 # main gui window(visible) sits on gui controller(not visible)
 root = Tk()
-# top menu bare(ie. File Edit View Options etc.) menu sits on main window
+
+
+"""
+The following section deals exclusively with the main menu. 
+These areas are where the File Edit View Help etc, menu sections will be
+created and handled, as well as the Status bar along the bottom of the main program window.
+"""
+# top menu bar(ie. File Edit View Options etc.) menu sits on main window
 main_menu = Menu(root)
+
+'''File Menu'''
 # dropdown menu from main menu
 file_menu = Menu(main_menu, tearoff=0)
 # makes a drop down File menu option
 main_menu.add_cascade(label='File', menu=file_menu)
 # Open File menu option in the File menu. Saves file path for manipulation later.
-file_path = file_menu.add_command(label='Open File', command=filedialog.askopenfilename)
+file_path = file_menu.add_command(label='Open File', command='')
 # adds a seperator between menu options
 file_menu.add_separator()
 # close program
 file_menu.add_command(label='Exit', command=root.quit)
-
 # not too sure about this line, but menus dont work without it.
 root.config(menu=main_menu)
+
+
+'''Status bar'''
+cur_status = 'Ready'
+status = Label(root, text=cur_status, bd='1', relief='sunken', anchor='w')
+status.pack(side='bottom', fill='x')
+
+
 # main program loop
 root.mainloop()
